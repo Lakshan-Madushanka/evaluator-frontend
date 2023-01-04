aa282ed9e2dddac311646ce55389a1346eb9ed49
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-04 12:51:17 +0530

    Install and setup tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "primeicons/primeicons.css"; //icons
 import "primevue/resources/primevue.min.css"; //core css
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "./assets/main.css";
+import "./assets/tailwind.css";
 
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
