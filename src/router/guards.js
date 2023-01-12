e926105fb229658ff2b04e87a975d63dd7b4d775
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-12 17:18:09 +0530

    Improve auth guard

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,9 +1,14 @@
 import router from "./index";
 import { useAppStore } from "@/stores/app";
+import { useAuthStore } from "@/stores/auth";
 
-export const auth = () => {
+export const auth = async () => {
   const appStore = useAppStore();
+  const authStore = useAuthStore();
 
+  if (appStore.authLoadedStatus === "error") {
+    await authStore.loadAuthUser();
+  }
   if (appStore.authenticated) {
     return true;
   } else {
