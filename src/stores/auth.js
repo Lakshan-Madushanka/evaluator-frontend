4ea15f66d6c0e9f3b3f201cd99a82ccd815233ae
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-12 17:17:40 +0530

    wip auth.js store

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,30 +2,106 @@ import * as authRequests from "../api/requests/auth";
 
 import { reactive, ref } from "vue";
 
+import { ROLES } from "@/constants";
 import { defineStore } from "pinia";
+import { useAppStore } from "./app";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore("auth", () => {
+  const router = useRouter();
+
+  const appStore = useAppStore();
+
+  const status = ref("");
   const loading = ref(false);
   const errors = ref({});
-  const data = reactive({ id: "", name: "", role: "", email: "" });
+  const user = reactive({ id: "", name: "", role: "", email: "" });
 
   async function login(payload) {
     loading.value = true;
     try {
       const response = await authRequests.loginRequest(payload);
-      setData(response);
+      setUser(response);
+      appStore.status = "";
+      appStore.authenticated = true;
+      errors.value = {};
+      router.replace({ name: "home" });
     } catch (data) {
+      appStore.setToast("error", data.message);
       errors.value = data.errors;
     } finally {
       loading.value = false;
     }
   }
 
-  function setData(response) {
-    data.id = response.id;
-    data.name = response.attributes.name;
-    data.email = response.attributes.email;
-    data.role = response.attributes.role;
+  async function update(payload) {
+    const newPayload = { ...payload, role: ROLES[user.role] };
+    status.value = "updating";
+    try {
+      const response = await authRequests.updateProfileRequest(
+        user.id,
+        newPayload
+      );
+      setUser(response);
+      errors.value = {};
+      appStore.setToast("success", "Profile updated successfully");
+    } catch (data) {
+      appStore.setToast("error", data.message);
+      errors.value = data.errors;
+    } finally {
+      status.value = "";
+    }
   }
-  return { login, loading, data, errors };
+
+  async function loadAuthUser() {
+    try {
+      const response = await authRequests.loadAuthUserRequest();
+
+      setUser(response);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  async function logOut() {
+    appStore.status = "loggingOut";
+    try {
+      const response = await authRequests.logOut();
+      appStore.authenticated = false;
+      resetState(response);
+
+      router.replace({ name: "home" });
+    } catch (error) {
+      //
+    } finally {
+      appStore.status = "";
+    }
+  }
+
+  function setUser(response) {
+    user.id = response.id;
+    user.name = response.attributes.name;
+    user.email = response.attributes.email;
+    user.role = response.attributes.role;
+  }
+
+  function resetState() {
+    user.id = "";
+    user.name = "";
+    user.email = "";
+    user.role = "";
+    errors.value = {};
+    loading.value = false;
+  }
+  return {
+    login,
+    update,
+    logOut,
+    loadAuthUser,
+    resetState,
+    status,
+    loading,
+    user,
+    errors,
+  };
 });
