bb0384b018546bb854c801797fa738845afba3b5
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-12 17:30:17 +0530

    Add missing stuffs in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+// Tailwind styles
 import "./assets/tailwind.css";
+// Prime Vue styles
+import "nprogress/nprogress.css";
 import "primeicons/primeicons.css"; //icons
 import "primevue/resources/primevue.min.css"; //core css
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
@@ -6,14 +9,20 @@ import "./assets/main.css";
 
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
+import Tooltip from "primevue/tooltip";
 import { createApp } from "vue";
+// Main styles
 import { createPinia } from "pinia";
 import router from "./router";
 
 const app = createApp(App);
 
+app.directive("tooltip", Tooltip);
+
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, { ripple: true, inputStyle: "filled" });
+app.use(ToastService);
 
 app.mount("#app");
