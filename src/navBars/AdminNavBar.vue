e74a78c9046cfc2a8d4c9877d51e90a58ec1a371
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-16 15:24:32 +0530

    Admin layout

---
+++ src/navBars/AdminNavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav
+    class="py-4 px-4 flex justify-between text-[#495057] bg-[#f8f9fa] border border-solid rounded"
+  >
+    <div>
+      <Breadcrumb :home="home" :model="getItems()" />
+    </div>
+    <div><AuthUserMenu /></div>
+  </nav>
+</template>
+
+<script setup>
+import Breadcrumb from "primevue/breadcrumb";
+import AuthUserMenu from "@/components/AuthUserMenu.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const home = { icon: "pi pi-th-large", to: { name: "admin.dashboard" } };
+
+function getItems() {
+  switch (route.name) {
+    default:
+      return null;
+  }
+}
+</script>
---
+++ src/views/layouts/AdminLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="relative flex">
+    <div>
+      <sidebar-menu
+        :menu="menu"
+        class="shadow-lg"
+        theme="white-theme"
+        @update:collapsed="onToggleCollapse"
+      >
+      </sidebar-menu>
+    </div>
+    <div
+      :class="[
+        'pl-[290px] w-full transition-all duration-[.3s]',
+        { '!pl-[65px]': sideBarCollapse },
+      ]"
+    >
+      <SessionTimeoutMessage />
+      <NavBar />
+      <div class="p-4">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { formatText } from "@/helpers";
+import { SidebarMenu } from "vue-sidebar-menu";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
+import NavBar from "@/navBars/AdminNavBar.vue";
+import { useAppStore } from "@/stores/app";
+import { useAuthStore } from "@/stores/auth";
+import Divider from "primevue/divider";
+import SessionTimeoutMessage from "@/components/SessionTimeoutMessage.vue";
+export default {
+  components: { SidebarMenu, NavBar, SessionTimeoutMessage },
+  setup() {
+    const appStore = useAppStore();
+    const authStore = useAuthStore();
+    const sideBarCollapse = ref(false);
+
+    const menu = [
+      {
+        header: formatText(`😊 Hello ${authStore.user.name}`, 20),
+        hiddenOnCollapse: true,
+        class: "text-center !text-xl !normal-case !pt-6",
+        icon: "pi pi-fw pi-th-large",
+      },
+      {
+        component: Divider,
+      },
+      {
+        href: { name: "home" },
+        title: "Home",
+        icon: "pi pi-fw pi-home",
+        exact: true,
+      },
+      {
+        href: { name: "admin.dashboard" },
+        title: "Dashboard",
+        icon: "pi pi-fw pi-th-large",
+        exact: true,
+      },
+      {
+        component: Divider,
+      },
+    ];
+
+    function onToggleCollapse(event) {
+      sideBarCollapse.value = event;
+    }
+
+    return { appStore, menu, onToggleCollapse, sideBarCollapse };
+  },
+};
+</script>
+
+<style>
+.av-sidebar-menu .vsm--menu .vsm--header {
+  color: rgb(202, 21, 21);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
