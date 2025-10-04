31e8f3f03abb72bf26470b014c62b4eed251ecee
Lakshan-Madushanka <Lakshan-Madushanka@users.noreply.github.com>
2025-10-04 05:42:32 +0000

    Prettified Code!

--- src/views/admin/teams/questionnaires/IndexView.vue
+++ src/views/admin/teams/questionnaires/IndexView.vue
@@ -42,7 +42,7 @@
                         :class="
                           columnVisibility[
                             snake(
-                              lowercaseFirstLetter(slotProps['item']['label'])
+                              lowercaseFirstLetter(slotProps['item']['label']),
                             ).toLowerCase()
                           ]
                             ? 'pi pi-eye'
@@ -168,7 +168,7 @@
             <template #body="slotProps">
               {{
                 moment(slotProps.data.attributes.created_at).format(
-                  "ddd, MMM D, yyyy, h:mm a"
+                  "ddd, MMM D, yyyy, h:mm a",
                 )
               }}</template
             >
